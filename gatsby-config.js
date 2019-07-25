522d4c0b65e18375cc6f784a780b64fc34397ad9
Eason Chiang <ishiu.chiang@gmail.com>
2019-07-25 22:00:58 +0800

    revise the project infomation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Retro Free Cell`,
+    description: `復古風新接龍 | Designed by cyc`,
+    author: `@EasonChiang7178`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
