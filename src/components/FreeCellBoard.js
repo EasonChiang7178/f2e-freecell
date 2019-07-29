ddb89a580bb97f2e9456421dd355daee0c2d6b73
Eason Chiang <ishiu.chiang@gmail.com>
2019-07-29 10:27:47 +0800

    move game state to top level component

--- src/components/FreeCellBoard.js
+++ src/components/FreeCellBoard.js
@@ -2,10 +2,23 @@ import React from "react"
 import FreeCellCanvas from "./FreeCellCanvas"
 
 class FreeCellBoard extends React.PureComponent {
-  render = () => {
+  state = {
+    gameState: [
+      [{ id: 'heart_k' }, { id: 'club_2' }, { id: 'diamond_10' }, { id: 'heart_3' }, { id: 'heart_6' }, { id: 'spade_a' }, { id: 'spade_10' }],
+      [{ id: 'diamond_2' }, { id: 'spade_7' }, { id: 'diamond_6' }, { id: 'spade_5' }, { id: 'club_10' }, { id: 'diamond_k' }, { id: 'heart_8' }],
+      [{ id: 'spade_9' }, { id: 'heart_4' }, { id: 'club_q' }, { id: 'spade_4' }, { id: 'club_7' }, { id: 'spade_2' }, { id: 'club_9' }],
+      [{ id: 'heart_a' }, { id: 'spade_8' }, { id: 'diamond_4' }, { id: 'heart_j' }, { id: 'heart_q' }, { id: 'diamond_9' }, { id: 'spade_q' }],
+      [{ id: 'club_8' }, { id: 'diamond_q' }, { id: 'heart_9' }, { id: 'club_k' }, { id: 'spade_k' }, { id: 'diamond_a' }],
+      [{ id: 'heart_10' }, { id: 'club_a' }, { id: 'diamond_7' }, { id: 'heart_2' }, { id: 'club_j' }, { id: 'spade_6' }],
+      [{ id: 'club_6' }, { id: 'diamond_3' }, { id: 'heart_5' }, { id: 'diamond_j' }, { id: 'diamond_5' }, { id: 'heart_7' }],
+      [{ id: 'spade_3' }, { id: 'diamond_8' }, { id: 'club_5' }, { id: 'club_3' }, { id: 'spade_j' }, { id: 'club_4' }],
+    ],
+    history: []
+  }
 
+  render = () => {
     return (
-      <FreeCellCanvas />
+      <FreeCellCanvas state={this.state.gameState} />
     )
   }
 }
--- src/components/FreeCellCanvas/index.js
+++ src/components/FreeCellCanvas/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Stage, Layer } from 'react-konva/lib/ReactKonvaCore'
 
 import ImageDataConsumer, { ImageDataProvider } from '../../contexts/ImageDataContext'
@@ -8,6 +9,9 @@ import SolvedDeck from './decks/SolvedDeck'
 import PuzzleBoard from './PuzzleBoard'
 
 class FreeCellCanvas extends React.PureComponent {
+  static propTypes = {
+    state: PropTypes.array.isRequired
+  }
 
   getCanvasWidth = () => 1440 // [TODO] Support SSR and responsive simultaneously
 
@@ -23,16 +27,7 @@ class FreeCellCanvas extends React.PureComponent {
               <SolvedDeck />
 
               <PuzzleBoard
-                deckOfCards={[
-                  [{ id: 'heart_k' }, { id: 'club_2' }, { id: 'diamond_10' }, { id: 'heart_3' }, { id: 'heart_6' }, { id: 'spade_a' }, { id: 'spade_10' }],
-                  [{ id: 'diamond_2' }, { id: 'spade_7' }, { id: 'diamond_6' }, { id: 'spade_5' }, { id: 'club_10' }, { id: 'diamond_k' }, { id: 'heart_8' }],
-                  [{ id: 'spade_9' }, { id: 'heart_4' }, { id: 'club_q' }, { id: 'spade_4' }, { id: 'club_7' }, { id: 'spade_2' }, { id: 'club_9' }],
-                  [{ id: 'heart_a' }, { id: 'spade_8' }, { id: 'diamond_4' }, { id: 'heart_j' }, { id: 'heart_q' }, { id: 'diamond_9' }, { id: 'spade_q' }],
-                  [{ id: 'club_8' }, { id: 'diamond_q' }, { id: 'heart_9' }, { id: 'club_k' }, { id: 'spade_k' }, { id: 'diamond_a' }],
-                  [{ id: 'heart_10' }, { id: 'club_a' }, { id: 'diamond_7' }, { id: 'heart_2' }, { id: 'club_j' }, { id: 'spade_6' }],
-                  [{ id: 'club_6' }, { id: 'diamond_3' }, { id: 'heart_5' }, { id: 'diamond_j' }, { id: 'diamond_5' }, { id: 'heart_7' }],
-                  [{ id: 'spade_3' }, { id: 'diamond_8' }, { id: 'club_5' }, { id: 'club_3' }, { id: 'spade_j' }, { id: 'club_4' }],
-                ]}
+                deckOfCards={this.props.state}
               />
             </Layer>
           </ImageDataProvider>
