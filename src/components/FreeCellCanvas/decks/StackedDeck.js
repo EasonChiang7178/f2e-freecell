efa5b20c4849515a0f4597fc37b6a71a55549307
Eason Chiang <ishiu.chiang@gmail.com>
2019-07-30 15:15:00 +0800

    return empty cell when stackedDeck is empty

--- src/components/FreeCellCanvas/decks/StackedDeck.js
+++ src/components/FreeCellCanvas/decks/StackedDeck.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Group } from 'react-konva/lib/ReactKonvaCore'
+import { Group, Rect } from 'react-konva/lib/ReactKonvaCore'
+import "konva/lib/shapes/Rect"
 
 import Card from "../cells/Card"
 import Freecell from "../../../utils/freecell"
@@ -28,9 +29,15 @@ class StackedDeck extends React.PureComponent {
     return !dragDisabled && Freecell.isCardInStackDraggable(cards, i)
   }
 
-  renderCards = () => this.props.cards.map((card, i) => (
-    <Card id={card.id} key={card.id} x={0} y={STACKED_CARD_OFFSET_Y * i} draggable={this.isCardDraggable(i)} />
-  ))
+  renderCards = () => {
+    const { cards } = this.props
+    
+    return cards.length > 0 ? cards.map((card, i) => (
+      <Card id={card.id} key={card.id} x={0} y={STACKED_CARD_OFFSET_Y * i} draggable={this.isCardDraggable(i)} />
+    )) : (
+      <Rect width={100} height={150} cornerRadius={5} fill="transparent" stroke="transparent" strokeWidth={0} />
+    )
+  }
 
   render = () => {
     const { x, y, setRef } = this.props
